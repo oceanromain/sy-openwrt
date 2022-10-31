6709d706d678cb6a0631251f7a4016a9fbae2311
oceanromain <68986251+oceanromain@users.noreply.github.com>
2022-10-31 17:29:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git nas https://github.com/linkease/nas-packages.git' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git poweroff https://github.com/sirpdboy/luci-app-poweroffdevice' >>feeds.conf.default
+echo 'src-git luci-app-ddns-go https://github.com/sirpdboy/luci-app-ddns-go' >>feeds.conf.default
