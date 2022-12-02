25e85c8bb50b920666bbb7450153c0d4f69be7c3
oceanromain <starinternet@163.com>
2022-12-02 16:49:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,3 +21,4 @@ echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >>feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >>feeds.conf.default
 echo 'src-git ddns_go https://github.com/sirpdboy/luci-app-ddns-go' >>feeds.conf.default
+echo 'src-git autocore https://github.com/sirpdboy/myautocore package/myautocore' >>feeds.conf.default
